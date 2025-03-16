522d3442f3ff0f98b1d63bd7cb5413018ae5dfdf
Kirill Donetskov <kirilldonetskov@gmail.com>
2025-03-17 03:29:30 +0600

    Updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from calc import Calculator
+from window import Window
+from controller import CalculatorController
+from PyQt6.QtCore import QFile, QTextStream
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    MainWindow = Calculator()
-    MainWindow.show()
+
+    file = QFile("C:\Coding\Python\OOP\calculator\style.css")
+    if file.open(QFile.OpenModeFlag.ReadOnly | QFile.OpenModeFlag.Text):
+        stream = QTextStream(file)
+        app.setStyleSheet(stream.readAll())
+    else:
+        print("Failed to load stylesheet")
+
+    model = Calculator() 
+    view = Window() 
+    controller = CalculatorController(model, view) 
+
+    view.show()
+
     sys.exit(app.exec())
