8a2b57d46fecaf76efa909f75d9044f3ef77b552
Kirill Donetskov <kirilldonetskov@gmail.com>
2025-03-17 02:56:35 +0600

    Updated logic part

--- calc.py
+++ calc.py
@@ -1,10 +1,25 @@
-from PyQt6.QtWidgets import QMainWindow, QGraphicsBlurEffect, QApplication, QDialog, QLabel, QPushButton, QVBoxLayout
-
-class Calculator(QMainWindow):
+class Calculator:
     def __init__(self):
-        super().__init__()
-        self.initUI()
-        
-    def initUI(self):
-        self.setGeometry(100, 100, 800, 400)
-        self.setWindowTitle("Calculator")
+        self.expression = ""
+
+    def add_to_expression(self, char):
+        self.expression += char
+
+    def remove_last_character(self):
+        self.expression = self.expression[:-1]
+
+    def clear_expression(self):
+        self.expression = ""
+
+    def calculate(self):
+        try:
+            result = round(eval(self.expression), 3)
+            self.expression = str(result)
+            return result
+        except ZeroDivisionError as e:
+            return "You can't divide by zero!"
+        except Exception as e:
+            return "Invalid syntax!"
+
+    def get_expression(self):
+        return self.expression
